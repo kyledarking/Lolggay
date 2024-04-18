86b6f8b8a3315d7bd24d60604d0b9015844e2223
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-18 15:04:55 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100042484217511",];
+    const permission = ["100090775159086",];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
     }
